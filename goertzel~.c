5028372f63970d90ec95e3cb0d8af9c7c244ed57
Ben Wesch <benjamin.wesch@gmail.com>
2024-10-02 21:57:42 +0200

    working version without windowing

--- goertzel~.c
+++ goertzel~.c
@@ -5,84 +5,82 @@ static t_class *goertzel_tilde_class;
 
 typedef struct _goertzel_tilde {
     t_object  x_obj;
-    t_sample f;
-    t_outlet *x_out;
-    t_float coeff;
-    t_float q0;
-    t_float q1;
-    t_float q2;
-    t_float sine;
-    t_float cosine;
-    t_float frequency;
-    t_float sr;
+    t_sample f_dummy;     // Dummy float for signal inlet
+    t_float f_frequency;  // Target frequency
+    t_int   f_n;          // Block size
+    t_outlet *x_out;      // Float outlet
 } t_goertzel_tilde;
 
 static t_int *goertzel_tilde_perform(t_int *w)
 {
     t_goertzel_tilde *x = (t_goertzel_tilde *)(w[1]);
     t_sample *in = (t_sample *)(w[2]);
-    t_sample *out = (t_sample *)(w[3]);
-    int n = (int)(w[4]);
+    int n = (int)(w[3]);
     
-    t_float q0 = x->q0;
-    t_float q1 = x->q1;
-    t_float q2 = x->q2;
-    t_float coeff = x->coeff;
+    float floatnumSamples = (float)n;
+    float k = (int)(0.5 + ((floatnumSamples * x->f_frequency) / sys_getsr()));
+    float omega = (2.0 * M_PI * k) / floatnumSamples;
+    float sine = sinf(omega);
+    float cosine = cosf(omega);
+    float coeff = 2.0 * cosine;
+    float q0 = 0, q1 = 0, q2 = 0;
     
-    while (n--) {
-        q0 = coeff * q1 - q2 + *in++;
+    // Process the block
+    for(int i = 0; i < n; i++)
+    {
+        q0 = coeff * q1 - q2 + in[i];
         q2 = q1;
         q1 = q0;
-        
-        *out++ = q0;
     }
     
-    x->q0 = q0;
-    x->q1 = q1;
-    x->q2 = q2;
+    // Calculate magnitude
+    float scalingFactor = n / 2.0;
+    float real = (q1 * cosine - q2) / scalingFactor;
+    float imag = (q1 * sine) / scalingFactor;
+    float magnitude = sqrtf(real*real + imag*imag);
     
-    return (w+5);
+    // Output the magnitude
+    outlet_float(x->x_out, magnitude);
+    
+    return (w+4);
 }
 
 static void goertzel_tilde_dsp(t_goertzel_tilde *x, t_signal **sp)
 {
-    dsp_add(goertzel_tilde_perform, 4, x, sp[0]->s_vec, sp[1]->s_vec, sp[0]->s_n);
+    x->f_n = sp[0]->s_n;
+    dsp_add(goertzel_tilde_perform, 3, x, sp[0]->s_vec, sp[0]->s_n);
 }
 
 static void goertzel_tilde_frequency(t_goertzel_tilde *x, t_floatarg f)
 {
-    x->frequency = f;
-    t_float omega = 2.0 * M_PI * x->frequency / x->sr;
-    x->coeff = 2.0 * cosf(omega);
-    x->sine = sinf(omega);
-    x->cosine = cosf(omega);
+    if (f > 0 && f < sys_getsr() / 2) {
+        x->f_frequency = f;
+    } else {
+        pd_error(x, "goertzel~: frequency must be positive and below Nyquist");
+    }
 }
 
 static void *goertzel_tilde_new(t_floatarg f)
 {
     t_goertzel_tilde *x = (t_goertzel_tilde *)pd_new(goertzel_tilde_class);
     
-    x->sr = sys_getsr();
-    x->frequency = f;
+    x->f_frequency = (f > 0 && f < sys_getsr() / 2) ? f : 440;
+    x->f_n = 64;  // Will be updated in _dsp method
     
-    inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_float, gensym("frequency"));
-    x->x_out = outlet_new(&x->x_obj, &s_signal);
-    
-    goertzel_tilde_frequency(x, f);
-    
+    x->x_out = outlet_new(&x->x_obj, &s_float);
     return (void *)x;
 }
 
-void goertzel_tilde_setup(void) {
+void goertzel_tilde_setup(void)
+{
     goertzel_tilde_class = class_new(gensym("goertzel~"),
         (t_newmethod)goertzel_tilde_new,
-        0, sizeof(t_goertzel_tilde),
+        0,
+        sizeof(t_goertzel_tilde),
         CLASS_DEFAULT,
         A_DEFFLOAT, 0);
     
-    class_addmethod(goertzel_tilde_class,
-        (t_method)goertzel_tilde_dsp, gensym("dsp"), A_CANT, 0);
-    class_addmethod(goertzel_tilde_class,
-        (t_method)goertzel_tilde_frequency, gensym("frequency"), A_FLOAT, 0);
-    CLASS_MAINSIGNALIN(goertzel_tilde_class, t_goertzel_tilde, f);
+    class_addmethod(goertzel_tilde_class, (t_method)goertzel_tilde_dsp, gensym("dsp"), A_CANT, 0);
+    class_addmethod(goertzel_tilde_class, (t_method)goertzel_tilde_frequency, gensym("frequency"), A_FLOAT, 0);
+    CLASS_MAINSIGNALIN(goertzel_tilde_class, t_goertzel_tilde, f_dummy);
 }
